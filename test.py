81b471f06e6c4fb12c81ce2a772b7a955a1929e0
jean-paul-mg <todisoajeanpaul33@gmail.com>
2024-08-27 14:39:51 +0300

    message

---
+++ test.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("300x200")
+label = tk.Label(root, text="salama tody", font=("Helvetica", 16))
+label.pack(pady=50)
+
+root.mainloop()
+root.title("test de GUI")
